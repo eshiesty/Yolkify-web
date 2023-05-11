6501294326767efed323f5eb44bbafb88dd8f0d3
Ellis O'Dowd <riptideellis@gmail.com>
2023-05-11 01:58:13 -0700

    added a lil mobile support

--- src/app/components/LandingPage/index.js
+++ src/app/components/LandingPage/index.js
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 import styles from "./styles.module.css";
 import appstore from "../../assets/appstore.png";
 import googleplay from "../../assets/googleplay.png";
@@ -6,40 +6,80 @@ import iosdashboard from "../../assets/screenshots/iosdash.png";
 import iosactive from "../../assets/screenshots/iosactive.png";
 import iosprs from "../../assets/screenshots/iosprs.png";
 import Image from "next/image";
+import useDeviceDetect from "../../utils";
+
 const LandingPage = () => {
   return (
-    <div className={styles.page}>
-      <div className={styles.titleHolder}>
-        <text className={styles.maintext}>
-          It's time to get{" "}
-          <text className={styles.emphasis}>
-            <br />
-            yolked
-          </text>
-          .
-        </text>
-      </div>
-      <div className={styles.screenshotArea}>
-        <div className={styles.screenshots}>
-          <Image src={iosdashboard} width={250} alt="yolkify dashboard" />
-          <Image src={iosactive} width={250} alt="yolkify workout" />
+    <div>
+      {useDeviceDetect().isMobile ? (
+        <div className={styles.pageMobile}>
+          <div className={styles.titleHolderMobile}>
+            <text className={styles.maintextMobile}>
+              It's time to get
+              <text className={styles.emphasisMobile}>
+                <br />
+                yolked
+              </text>
+              .
+            </text>
+          </div>
+          <div className={styles.screenshotAreaMobile}>
+            <div className={styles.screenshotsMobile}>
+              <Image src={iosdashboard} width={250} alt="yolkify dashboard" />
+              {/* <Image src={iosactive} width={250} alt="yolkify workout" /> */}
 
-          <Image src={iosprs} width={250} alt="yolkify prs" />
+              {/* <Image src={iosprs} width={250} alt="yolkify prs" /> */}
+            </div>
+            <div className={styles.storeIconsMobile}>
+              <Image src={appstore} width={150} alt="app store icon" />
+              <Image width={150} src={googleplay} alt="play store icon" />
+            </div>
+            {/* <div className={styles.androidArea}>
+      <Image src={graphs} width={270} alt="yolkify charts" />
+    </div> */}
+          </div>
+          <div className={styles.linkHolderMobile}>
+            <div className={styles.linkMobile}>about</div>
+            <div className={styles.linkMobile}>contact</div>
+            <div className={styles.linkMobile}>legal</div>
+            <div className={styles.linkMobile}>help</div>
+          </div>
         </div>
-        <div className={styles.storeIcons}>
-          <Image src={appstore} width={200} alt="app store icon" />
-          <Image width={200} src={googleplay} alt="play store icon" />
+      ) : (
+        <div className={styles.page}>
+          <div className={styles.titleHolder}>
+            <text className={styles.maintext}>
+              It's time to get
+              <text className={styles.emphasis}>
+                <br />
+                yolked
+              </text>
+              .
+            </text>
+          </div>
+          <div className={styles.screenshotArea}>
+            <div className={styles.screenshots}>
+              <Image src={iosdashboard} width={250} alt="yolkify dashboard" />
+              <Image src={iosactive} width={250} alt="yolkify workout" />
+
+              <Image src={iosprs} width={250} alt="yolkify prs" />
+            </div>
+            <div className={styles.storeIcons}>
+              <Image src={appstore} width={200} alt="app store icon" />
+              <Image width={200} src={googleplay} alt="play store icon" />
+            </div>
+            {/* <div className={styles.androidArea}>
+        <Image src={graphs} width={270} alt="yolkify charts" />
+      </div> */}
+          </div>
+          <div className={styles.linkHolder}>
+            <div className={styles.link}>about</div>
+            <div className={styles.link}>contact</div>
+            <div className={styles.link}>legal</div>
+            <div className={styles.link}>help</div>
+          </div>
         </div>
-        {/* <div className={styles.androidArea}>
-          <Image src={graphs} width={270} alt="yolkify charts" />
-        </div> */}
-      </div>
-      <div className={styles.linkHolder}>
-        <div className={styles.link}>about</div>
-        <div className={styles.link}>contact</div>
-        <div className={styles.link}>legal</div>
-        <div className={styles.link}>help</div>
-      </div>
+      )}
     </div>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
 import LandingPage from "./components/LandingPage";
-
+import MobileLandingPage from "./components/MobileLandingPage";
+import useDeviceDetect from "./utils";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <LandingPage />
+      {useDeviceDetect() ? <LandingPage /> : <MobileLandingPage />}
     </main>
   );
 }
