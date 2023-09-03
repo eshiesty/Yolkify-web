7958317c4534aff54365ab0062d42fd46a9c3530
Ellis O'Dowd <riptideellis@gmail.com>
2023-09-03 13:19:12 -0700

    fixed

--- src/pages/404Page/index.js
+++ src/pages/404Page/index.js
@@ -1,11 +1,6 @@
 "use client";
 import styles from "./styles.module.css";
-import appstore from "../../assets/appstore.png";
-import googleplay from "../../assets/googleplay.png";
-import iosdashboard from "../../assets/screenshots/iosdash.png";
-import iosactive from "../../assets/screenshots/iosactive.png";
-import iosprs from "../../assets/screenshots/iosprs.png";
-import Image from "next/image";
+
 import useDeviceDetect from "../../utils";
 
 const PageNotFound = () => {
