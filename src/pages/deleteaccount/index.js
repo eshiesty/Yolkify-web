3dd3f35c8cf945ca0cd2027415d9b2dcada6430c
Ellis O'Dowd <riptideellis@gmail.com>
2023-09-09 17:07:38 -0700

    Added account deletion page to website

---
+++ src/pages/deleteaccount/index.js
@@ -0,0 +1,19 @@
+import styles from "./styles.module.css";
+
+const DeleteAccount = () => {
+  return (
+    <div>
+      <h1>Delete account</h1>
+      <p>
+        You can delete your account from the Yolkify app. Navigate to the stats
+        bottom tab, then the profile top tab, and click the settings icon in the
+        top right. Click the red "delete account" button, and all of your
+        account data as well as account will be deleted.
+      </p>
+      <h2>Request data deletion</h2>
+      <p>For any other data deletion inquries, contact yolkifyapp@gmail.com</p>
+    </div>
+  );
+};
+
+export default DeleteAccount;
